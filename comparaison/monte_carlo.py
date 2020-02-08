d836718c0009c9ddb98e2b206dae8b68ad9deedd
Cédric JUNG <cedced19@gmail.com>
2020-02-08 18:01:08 +0100

    Add illustration for monte carlo methode

--- comparaison/monte_carlo.py
+++ comparaison/monte_carlo.py
@@ -9,8 +9,7 @@ from math import pi,cos
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def calcul (N, draw):
+def calcul (N):
     result=[]
     succes=1
     for i in range (2,N):
@@ -18,10 +17,33 @@ def calcul (N, draw):
         y = rd.random()
         if(x * x + y * y <= 1):
             succes+=1
+            
         r=(4*succes)/i
         result.append(r)
     return result
 
+def illustration (N):
+    fig, main_ax = plt.subplots()
+    main_ax.set_ylim(0, 1)
+    main_ax.set_xlim(0, 1)
+    plt.axis('equal')
+    plt.axis([0, 1, 0, 1])
+    succes=1
+    for i in range (2,N):
+        x = rd.random()
+        y = rd.random()
+        if (x * x + y * y <= 1):
+            succes+=1
+            plt.scatter(x,y,color='black')
+        else:
+            plt.scatter(x,y,color='red')
+    approx=(4*succes)/N
+    plt.title('Illustration N=' + str(N) + ' pi≈' + str(approx))
+    plt.savefig('monte_carlo_illustration.png')
+    plt.show()
+
+
+illustration(1000)
 
 def show1(N):
     fig, main_ax = plt.subplots()
@@ -32,7 +54,7 @@ def show1(N):
     main_ax.set_ylabel('Approximation')
     X = [ i for i in range (2,N)]
     for i in range(5):
-        Y = calcul(N, False)
+        Y = calcul(N)
         plt.plot(X, Y)
     
     plt.plot(X, [ pi for i in range (2,N)],  '-r', label='π')
@@ -50,7 +72,7 @@ def show2(N):
     main_ax.set_ylabel('Approximation')
     X = [ i for i in range (2,N)]
     for i in range(5):
-        Y = calcul(N, False)
+        Y = calcul(N)
         plt.plot(X, Y)
     
     plt.plot(X, [ pi for i in range (2,N)],  '-r', label='π')
