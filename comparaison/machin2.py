c2ddccec8ee7167680bca280a2ce25a946f8d1d0
Cédric JUNG <cedced19@gmail.com>
2020-02-22 00:13:19 +0100

    Add pyplot

--- comparaison/machin2.py
+++ comparaison/machin2.py
@@ -2,7 +2,8 @@
 
 from time import time
 import os
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 def compare (calculated, prec):
     filename = os.path.abspath('../y-cruncher/10K/pi-dec-chudnovsky.txt')
@@ -11,13 +12,14 @@ def compare (calculated, prec):
     calculated=str(calculated)
     i=0
     while test and i<prec:
-        ref = f.read(1)
+        ref = str(f.read(1))
         if (ref == '.'):
-            ref = f.read(1)
+            ref = str(f.read(1))
         if(calculated[i]!=ref):
             test=False
         else:
             i+=1
+    f.close()
     return i
 
 def arctan(x, one):
@@ -73,39 +75,61 @@ def arctan_euler(x, one):
 
 
 # Formules de Machin https://fr.wikipedia.org/wiki/Formule_de_Machin
-def pi_machin(one):
+def machin(one):
     return 4*(4*arctan(5, one) - arctan(239, one))
-def pi_machin_euler(one):
+def machin_euler(one):
     return 4*(4*arctan_euler(5, one) - arctan_euler(239, one))
 
-def pi_gauss(one):
+def gauss(one):
     return 4*(12*arctan(18, one) + 8*arctan(57, one) - 5*arctan(239, one))
-def pi_gauss_euler(one):
+
+def gauss_euler(one):
     return 4*(12*arctan_euler(18, one) + 8*arctan_euler(57, one) - 5*arctan_euler(239, one))
 
+def ferguson(one):
+    return 4*(3*arctan(4, one) + arctan(20, one) + arctan(1985, one))
 
-for log10_digits in range(1,7):
-    digits = 10**log10_digits
+def ferguson_euler(one):
+    return 4*(3*arctan_euler(4, one) + arctan_euler(20, one) + arctan_euler(1985, one))
+
+def hutton(one):
+    return 4*(2*arctan(3, one) + arctan(7, one))
+
+def hutton_euler(one):
+    return 4*(2*arctan_euler(3, one) + arctan_euler(7, one))
+
+
+list_methods = ['machin','gauss','ferguson','hutton','machin_euler','gauss_euler','ferguson_euler','hutton_euler']
+
+def exec_method (name,n):
+    '''
+    Execute la méthode passée en paramètre et retourne le nombre de décimales `digits`, le temps d'execution `time`, et le nombre de décimales "juste" 
+    '''
+    digits = 10**n
     one = 10**digits
+    start=time()
+    pi=eval(name + '(' + str(one) +  ')')
+    stop=time()
+    return (digits,stop-start,compare(pi,digits))
 
-    start =time()
-    pi = pi_machin(one)
-    print(compare(pi,digits))
-    print("machin: digits",digits,"time",time()-start)
 
-    start =time()
-    pi = pi_machin_euler(one)
-    #print(pi)
-    print("machin euler: digits",digits,"time",time()-start)
+def lint_name(name):
+    return name.capitalize().replace('_e', ' E')
 
-    start =time()
-    pi = pi_gauss(one)
-    print("gauss: digits",digits,"time",time()-start)
+def graph(N):
+    fig, main_ax = plt.subplots()
+    main_ax.set_title('Méthodes de Machin et autres')
+    main_ax.set_xlabel('Nombre de décimales')
+    main_ax.set_ylabel('Temps de calcul')
+    X = [ 10**i for i in range (1,N)]
+    for method in list_methods:
+        time_list=[]
+        for i in range (1,N):
+            (digits,time,check)=exec_method(method,i)
+            time_list.append(time)
+        plt.plot(X,time_list,'o-', label=lint_name(method))
+    plt.legend(loc='upper left')
+    plt.savefig('machin2.png')
+    plt.show()
 
-    start =time()
-    pi = pi_gauss_euler(one)
-    #print(pi)
-    print("gauss euler: digits",digits,"time",time()-start)
-    #print(pi_ferguson(one))
-    #print(pi_hutton(one))
-    #print(pi_gauss(one))
+graph(5)
